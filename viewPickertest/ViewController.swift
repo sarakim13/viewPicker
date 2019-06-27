1b1e262aa17070d005eac46f6f896dec27cf7b9e
sarakim13 <sarakim0903@gmail.com>
2019-06-27 11:32:34 -0400

    done

--- viewPickertest/ViewController.swift
+++ viewPickertest/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let dataSource = ["toast", "avo", "chicken"]
+    private let ingred = ["whole wheat bread, butter" , "avocado, egg, hot sauce" , "chicken breast, herb"]
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    @IBOutlet weak var detailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        pickerView.dataSource = self as! UIPickerViewDataSource
+        pickerView.delegate = self
         // Do any additional setup after loading the view.
     }
 
 
 }
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        detailLabel.text = ingred[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dataSource[row]
+    }
+    }
 
